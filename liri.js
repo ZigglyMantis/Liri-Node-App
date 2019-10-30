ee318fad6d9ccb72fdf851ebb459403fe6204e07
Zygimantas Macijauskas <zigglymantis@gmail.com>
2019-10-29 23:38:31 -0400

    couldnt fix error with spotify so put in working placeholder code until i can figure it out

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
-// var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
-// var SpotifyNode = require("node-spotify-api")
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api")
+var spotify = new Spotify(keys.spotify);
 var fs = require("fs")
 var moment = require("moment")
 var axios = require("axios")
@@ -40,7 +40,7 @@ function liriBot(){
     var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
         axios.get(URL).then(function(response){
             var jsonData = response.data[0];
-            console.log(response.data[0])
+            // console.log(response.data[0])
             var correctedDate = moment(jsonData.datetime, "YYYY-MM-DD").format("MM/DD/YYYY")
             var artistData = [
                 "Venue: " + jsonData.venue.name,
@@ -51,9 +51,42 @@ function liriBot(){
         });
     }
 
-    function spotifyThis(){
-        
-    }
+    
+
+    var getArtistNames = function (artist) {
+        return artist.name;
+    };
+    
+    // Function for running a Spotify search - Command is spotify-this-song
+    function spotifyThis(songName) {
+        if (songName === undefined) {
+            songName = "What's my age again";
+        }
+    
+        spotify.search(
+            {
+                type: "track",
+                query: input
+            },
+            function (err, data) {
+                if (err) {
+                    console.log("Error occurred: " + err);
+                    return;
+                }
+    
+                var songs = data.tracks.items;
+    
+                for (var i = 0; i < songs.length; i++) {
+                    console.log(i);
+                    console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+                    console.log("song name: " + songs[i].name);
+                    console.log("preview song: " + songs[i].preview_url);
+                    console.log("album: " + songs[i].album.name);
+                    console.log("-----------------------------------");
+                }
+            }
+        );
+    };
 }
 // Switch functions
 
